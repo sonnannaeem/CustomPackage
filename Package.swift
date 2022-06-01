9f586f91aa55131bd9f10fc449bcf9c9cd11f7b0
Sonnan Naeem <sonnan.naeem@onymos.com>
2022-05-31 20:27:26 -0700

    manifest updated

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "CustomFramework",
+    name: "CustomPackage",
     platforms: [
       .macOS(.v12), .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CustomFramework",
+            name: "CustomPackage",
             targets: ["CustomFramework"]),
     ],
     dependencies: [
@@ -21,8 +21,8 @@ let package = Package(
     targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages this package depends on.
-            .binaryTarget(
-                name: "CustomFramework",
-                path: "./Sources/CustomFramework.xcframework")
+        .binaryTarget(
+            name: "CustomFramework",
+            path: "./Sources/CustomFramework.xcframework")
         ]
 )
